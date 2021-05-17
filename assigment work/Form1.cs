e958584fd9912cebc1be2cdc4b00f3cb55eec759
Samuel Thomalla <RC0043100@student.riversidecollege.ac.uk>
2021-05-17 15:32:02 +0100

    hh

--- assigment work/Form1.cs
+++ assigment work/Form1.cs
@@ -31,5 +31,53 @@ namespace assigment_work
         {
 
         }
+
+        private void T_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //read Height, Width and Length from textboxes
+                double Length = double.Parse(textBox1.Text);
+                double Height = double.Parse(textBox2.Text);
+                double Width = double.Parse(textBox2.Text);
+                //read Surface Area and Volume from labels 
+                double SurfaceArea = 2.0 * (Length * Height + Length * Width + Height * Width);
+                double Volume = Length * Height * Width;
+                //run the code between these brackets 
+                label1.Text = "Surface Area= " + SurfaceArea;
+                label2.Text = "Volume= " + Volume;
+            }
+            catch
+            {
+                MessageBox.Show("error on input");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //read Height, Width and Length from textboxes
+                double Length = double.Parse(textBox1.Text);
+                double Height = double.Parse(textBox2.Text);
+                double Width = double.Parse(textBox2.Text);
+                //read Surface Area and Volume from labels 
+                double SurfaceArea = 2.0 * (Length * Height + Length * Width + Height * Width);
+                double Volume = Length * Height * Width;
+                //run the code between these brackets 
+                label1.Text = "Surface Area= " + SurfaceArea;
+                label2.Text = "Volume= " + Volume;
+            }
+            catch
+            {
+                MessageBox.Show("error on input");
+            }
+        }
     }
 }
